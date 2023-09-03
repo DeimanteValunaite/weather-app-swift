c5bc57c0d14125701582a096251dd518f64ff2e3
Deimante Valunaite <deimante.valunaite@gmail.com>
2023-09-03 12:53:17 +0300

    Initial Commit

---
+++ WeatheryApp/WeatheryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheryAppApp.swift
+//  WeatheryApp
+//
+//  Created by Deimante Valunaite on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatheryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
